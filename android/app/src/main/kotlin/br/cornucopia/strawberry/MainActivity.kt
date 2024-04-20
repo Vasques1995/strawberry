ae3b41a9a6c77f38e5ecf85105d8159409fabcbb
Rodrigo Liporace <rodrigo.vasquestuf@gmail.com>
2024-04-20 12:16:07 -0300

    ⚙[CHORE]: Base VGV CLI project

---
+++ android/app/src/main/kotlin/br/cornucopia/strawberry/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.cornucopia.strawberry.strawberry
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:strawberry/app/app.dart';
+import 'package:strawberry/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
