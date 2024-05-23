9c1a1c10162311cdae1c958ce46a825b642acb46
Rodrigo Liporace <rodrigo.vasquestuf@gmail.com>
2024-05-23 11:07:42 -0300

    🍓 [FEATURE]: Adding routing with goRouter

---
+++ lib/app/routing/routes.dart
@@ -0,0 +1,6 @@
+class AppRoutes {
+  static const String home = 'home';
+  static const String onboarding = 'onboarding';
+  static const String statistics = 'statistics';
+  static const String error = 'error';
+}
